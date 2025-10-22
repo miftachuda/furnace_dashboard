ad45c1575dc2e905c26e1849bba915c5be421ba2
Miftachul Huda <miftachulcc@gmail.com>
2025-10-22 07:49:58 +0700

    ch tsrf

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -24,7 +24,7 @@ const Dashboard: React.FC = () => {
   const sg_fuel_gas = 0.7348;
   const sg_fuel_oil = 0.9184;
   const mass_air_per_kg = 1.225;
-  const tsrf_fuel_gas_fact = 1.025;
+  const tsrf_fuel_gas_fact = 0.945;
   const tsrf_fuel_oil_fact = 0.9671;
   const [datas, setDatas] = useState<TagData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
